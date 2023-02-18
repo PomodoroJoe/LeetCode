d151b6aa2dd4a16f750d07a4c16404ce1142d5dc
Pomodoro Joe <pomodoro.joey@gmail.com>
2023-02-18 13:29:08 -0800

    LeetCode 226

---
+++ solutions/226/leetcode_226.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------
+# Solution 1 - recursive
+#------------------------------------------------------
+
+# Runtime:  94.82%
+# Memory:   94.95%
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
