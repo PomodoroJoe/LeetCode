e5c39347b5a849a933bf400e35092fc4f01aed5f
Pomodoro Joe <pomodoro.joey@gmail.com>
2023-08-10 14:08:03 -0700

    LeetCode 81

---
+++ solutions/81/leetcode_81.py
@@ -0,0 +1,12 @@
+
+#------------------------------------------------------
+# Solution 1 - cheeky
+#------------------------------------------------------
+
+# Runtime:  93.10%
+# Memory:   77.70%
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        return target in nums
