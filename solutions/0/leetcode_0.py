8f262d271631516d2c95638b1c2bb3f3a903e242
Pomodoro Joe <pomodoro.joey@gmail.com>
2023-08-10 23:43:56 -0700

    LeetCode 0

---
+++ solutions/0/leetcode_0.py
@@ -0,0 +1,14 @@
+#------------------------------------------------------
+# Solution 0 = Placeholder
+#------------------------------------------------------
+
+These are not the solutions you are looking for...
+
+
+Sorry!  You probably got here by clicking on a link
+from my YouTube video, but...
+
+I haven't had a chance to post the solution you're
+looking for (or I just totally forgot).  Either way,
+sorry.  Sucks to be you. ;p
+
